9a42140f694e4d2298cf56e46e240a90046b5576
tim-hyon <tim.c.hyon@gmail.com>
2015-09-27 05:13:45 -0400

    Server updates expired contacts

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ var rest = require('restler');
 var async = require('async');
 
 var tiesRef = new Firebase("https://ties.firebaseio.com/");
+var base = "https://ties.firebaseio.com/Contacts"
 
 var count = 0;
 setInterval(function() {
   count += 1;
-  rest.get('https://ties.firebaseio.com/Contacts.json').on('complete', function(data) {
-    console.log(data); // auto convert to object
+  rest.get(base+'.json').on('complete', function(data) {
+    for (contact in data) {
+      console.log(data[contact]);
+      var seconds = ((new Date).getTime())/1000;
+      if (seconds > data[contact].expDate) {
+        var currRef = new Firebase(base+'/'+contact);
+        currRef.update({expired: true});
+      }
+    }
   });
-  console.log(count);
+  //console.log(count);
 }, 1000);
